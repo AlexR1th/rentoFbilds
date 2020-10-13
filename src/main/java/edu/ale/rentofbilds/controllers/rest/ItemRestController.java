11870bc8b2cbdf013042466154611641d7e5b0f9
AlexR1th <wandering123456@gmail.com>
2020-10-13 19:19:59 +0300

    Initial commit

---
+++ src/main/java/edu/ale/rentofbilds/controllers/rest/ItemRestController.java
@@ -0,0 +1,28 @@
+package edu.ale.rentofbilds.controllers.rest;
+
+import edu.ale.rentofbilds.model.Item;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.time.LocalDateTime;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+@RestController
+@RequestMapping("/api/item")
+public class ItemRestController {
+    List<Item> list = Stream.of(
+            new Item("1", "Coca-Cola", "Drink",
+                    LocalDateTime.now(), LocalDateTime.now()),
+            new Item("2", "Pepsi", "Drink",
+                    LocalDateTime.now(), LocalDateTime.now()),
+            new Item("3", "Sprite", "Drink",
+                    LocalDateTime.now(), LocalDateTime.now())
+    ).collect(Collectors.toList());
+
+    @RequestMapping("/all")
+    List<Item> getAll() {
+        return list;
+    }
+}
